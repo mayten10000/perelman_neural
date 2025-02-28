5c3e89eef17cf5435f43d247181e0cde330c0f3f
John9xDoe <100959916+John9xDoe@users.noreply.github.com>
2025-03-01 02:19:48 +0300

    Дебаг метода get_answer (2) и улучшение функциональности (1)
    Дебаг:
    - учтён случай с единичным коэффицентом (который принято опускать) перед x ;
    - учтён случай если a (сумма коэф-ов перед x) = b (сумма свободных членов) = 0 - Истина ;
    Улучшение функциональности - добавление записи в csv

--- gen_linear_equations.py
+++ gen_linear_equations.py
@@ -3,19 +3,13 @@ from math import *
 from fractions import Fraction as F
 
 def generate_linear_equation(n):
-
     parts = choice(['r', 'l', 'rl'])
 
     if parts == 'r':
-        
-        return get_answer(f'0 = {sign_fix(gen_polynomial(1, n + 1))}')
-    
+        return get_answer(f'0 = {sign_fix(gen_polynomial(1, n + 1))}')    
     elif parts == 'l':
-        
         return get_answer(f'{sign_fix(gen_polynomial(1, n + 1))} = 0')
-    
     else:
-        
         nl = randint(1, n - 1)
         nr = n - nl
         
@@ -32,31 +26,22 @@ def gen_polynomial(mn,mx):
     poly = ''
 
     for n0 in range(n):
-
         poly += (gen_monomial(mn_gen=1, mx_gen=10) + signs[n0])
     
     return poly
 
-
 def gen_monomial(mn_gen, mx_gen):
-
     free = choice([True, False])
 
     #mono = str(choice([randrange(mn_gen, 0),randrange(1, mx_gen)]))
     mono = str(randint(mn_gen,mx_gen))
     
     if free:
-
         return mono
-
     else:
-
         if choice(['*',' ']) == '*':
-
             return mono + '*x'
-
         else:
-
             return mono + 'x'
         
 def get_answer(s):
@@ -66,50 +51,36 @@ def get_answer(s):
     a, b = 0, 0 # ax + b = 0
     
     for m in range(0, monos.index('=')):
-        
-        if 'x' in monos[m]:
-            
+        if monos[m] == 'x':
+            a += 1
+        elif 'x' in monos[m]:
             if '*' in monos[m]:
-                
                 a += int(monos[m][:-2])
-                
             else:
-                
-                a += int(monos[m][:-1])
-                
+                a += int(monos[m][:-1])   
         else:
-            
             b -= int(monos[m])
             
     for m in range(monos.index('=') + 1, len(monos)):
-            
-        if 'x' in monos[m]:
-                
+        if monos[m] == 'x':
+            a -= 1
+        elif 'x' in monos[m]:
             if '*' in monos[m]:
-                    
-                a -= int(monos[m][:-2])
-                    
+                a -= int(monos[m][:-2])   
             else:
-                    
-                a -= int(monos[m][:-1])
-                    
+                a -= int(monos[m][:-1])       
         else:
-                
             b += int(monos[m])
             
     #print(monos, a, b)
             
     if a != 0:
-        
         if b != 0:
-
             return (s, F(b, a))
-        else:
-            
-            return (s, "Истина (любое число)")
-    
+        return (s, "Истина (любое число)")
     else:
-        
+        if b == 0:
+            return (s, "Истина (любое число)")
         return (s, "Ложь (пустое множество)")
     
     
@@ -118,5 +89,15 @@ sign_fix = lambda s: s.replace('+ -', '- ').replace('- +', '- ').replace('- -', 
 
 n = 4 # int(input())
 
-for _ in range(10):
-    print(*generate_linear_equation(n), sep=' | ')
+eqs = {}
+
+N = int(input("Введите кол-во генерируемых уравнений: "))
+
+for _ in range(N):
+    eq = generate_linear_equation(n)
+    print(*eq, sep=' | ')
+    eqs[eq[0]] = eq[1]
+
+df = pd.DataFrame(list(eqs.items()), columns=["Equations","Answers"])
+df.to_csv("equations (gen).csv", index=False, encoding="utf-8")
+print("✅ Уравнения сохранены в equations (gen).csv!")
